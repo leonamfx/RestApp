8dbe0a638312ba2dca01b6c242cbb12ddc9cbfdb
Leonam Lima <leonamfx@gmail.com>
2019-09-13 16:51:28 -0300

    Setup autorizacoes e dependencias

--- AndroidStudioProjects/RestApp/app/src/androidTest/java/leonamlima/com/restapp/ExampleInstrumentedTest.java
+++ AndroidStudioProjects/RestApp/app/src/androidTest/java/leonamlima/com/restapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package leonamlima.com.restapp;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- AndroidStudioProjects/RestApp/app/src/main/java/leonamlima/com/restapp/MainActivity.java
+++ AndroidStudioProjects/RestApp/app/src/main/java/leonamlima/com/restapp/MainActivity.java
@@ -1,6 +1,7 @@
 package leonamlima.com.restapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
